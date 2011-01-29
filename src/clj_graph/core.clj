e08ef623e8757e33498967a46a78e4ee4ad0b55a
Eric Lavigne <lavigne.eric@gmail.com>
2011-01-29 12:40:32 -0500

    Added density calculation.

--- src/clj_graph/core.clj
+++ src/clj_graph/core.clj
@@ -5,21 +5,25 @@
 
 (defn random-graph
   "Generate a random graph with a given number of nodes and approximate density.
+    All configuration parameters are optional, so random-graph can be called
+    with no arguments.
 
     Example:
         (random-graph {:num-nodes 5 :density 0.5})
-          => {0 #{},
-              1 #{},
-              2 #{},
-              3 #{},
-              4 #{}}"
+          => {0 #{1 4},
+              1 #{0 3},
+              2 #{3},
+              3 #{1 2 4},
+              4 #{0 3}}"
   ([] (random-graph {}))
   ([options]
      (let [num-nodes (or (:num-nodes options) 10)
            nodes (range num-nodes)
            possible-edges (combinations nodes 2)
+           possible-edges (if possible-edges possible-edges [])
            medium-density (min 1.0 (/ (expt num-nodes 4/3)
-                                      (count possible-edges) 2))
+                                      (max 1 (count possible-edges))
+                                      2))
            density (or (:density options) medium-density)
            _ (if (or (> 0.0 density) (< 1.0 density))
                (throw (IllegalArgumentException.
@@ -34,3 +38,18 @@
                               b #{a}}))
                graph-without-edges
                edges))))
+
+(defn- sum [col]
+  (reduce + 0 col))
+
+(defn density
+  "Calculate the density of a graph. Density is proportional to the number of
+   edges. A graph with density 0 has no edges. A graph with density 1 has each
+   node connected to every other node. Densities outside the range [0,1] are
+   not possible."
+  [graph]
+  (let [num-nodes (count graph)
+        num-edges (sum (map count (vals graph)))
+        num-possible-edges (* num-nodes (dec num-nodes))]
+    (if (< num-nodes 2) 1.0 (/ (* 1.0 num-edges)
+                               num-possible-edges))))
--- test/clj_graph/test/core.clj
+++ test/clj_graph/test/core.clj
@@ -1,10 +1,32 @@
 (ns clj-graph.test.core
   (:use [clj-graph.core] :reload)
-  (:use [clojure.test]))
+  (:use [clojure.test])
+  (:use clojure.contrib.math))
+
+(defn approx= [tolerance]
+  (fn [& args]
+    (if (< (count args) 2)
+      true
+      (> tolerance (abs (- (apply max args)
+                           (apply min args)))))))
 
 (deftest test-random
   (is (< 0 (count (random-graph)))
       (str "random-graph creates a graph with at least one node "
            "when no options specified."))
   (is (= 20 (count (random-graph {:num-nodes 20})))
-      "random-graph creates a graph with the specified number of nodes."))
+      "random-graph creates a graph with the specified number of nodes.")
+  (is (= 0 (count (random-graph {:num-nodes 0})))
+      "Possible to create random-graph with no nodes.")
+  (is (= 1 (count (random-graph {:num-nodes 1})))
+      "Possible to create random-graph with only one node.")
+  (is (= 2 (count (random-graph {:num-nodes 2})))
+      "Possible to create random-graph with only two nodes."))
+
+(deftest test-density
+  (is ((approx= 0.05) 0.4 (density (random-graph {:num-nodes 100 :density 0.4})))
+      "Density of random-graph approximately matches requested density.")
+  (is (and (density (random-graph {:num-nodes 0}))
+           (density (random-graph {:num-nodes 1}))
+           (density (random-graph {:num-nodes 2})))
+      "Can calculate densities of very small graphs."))
