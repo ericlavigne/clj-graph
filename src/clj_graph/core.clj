2d7b45470b705d2e07176aa54c561fbbfdeadce9
Eric Lavigne <lavigne.eric@gmail.com>
2011-01-29 11:35:43 -0500

    Created a random graph generator, which should be useful for testing.

--- src/clj_graph/core.clj
+++ src/clj_graph/core.clj
@@ -1,1 +1,36 @@
-(ns clj-graph.core)
+(ns clj-graph.core
+  (:use clojure.contrib.combinatorics
+        clojure.contrib.math
+        clojure.set))
+
+(defn random-graph
+  "Generate a random graph with a given number of nodes and approximate density.
+
+    Example:
+        (random-graph {:num-nodes 5 :density 0.5})
+          => {0 #{},
+              1 #{},
+              2 #{},
+              3 #{},
+              4 #{}}"
+  ([] (random-graph {}))
+  ([options]
+     (let [num-nodes (or (:num-nodes options) 10)
+           nodes (range num-nodes)
+           possible-edges (combinations nodes 2)
+           medium-density (min 1.0 (/ (expt num-nodes 4/3)
+                                      (count possible-edges) 2))
+           density (or (:density options) medium-density)
+           _ (if (or (> 0.0 density) (< 1.0 density))
+               (throw (IllegalArgumentException.
+                       (str "Density (" density ") should be between 0 and 1."))))
+           edges (take (int (* density (count possible-edges)))
+                       (shuffle possible-edges))
+           graph-without-edges (zipmap nodes (repeat #{}))]
+       (reduce (fn [graph [a b]]
+                 (merge-with union
+                             graph
+                             {a #{b}
+                              b #{a}}))
+               graph-without-edges
+               edges))))
--- test/clj_graph/test/core.clj
+++ test/clj_graph/test/core.clj
@@ -2,5 +2,9 @@
   (:use [clj-graph.core] :reload)
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest test-random
+  (is (< 0 (count (random-graph)))
+      (str "random-graph creates a graph with at least one node "
+           "when no options specified."))
+  (is (= 20 (count (random-graph {:num-nodes 20})))
+      "random-graph creates a graph with the specified number of nodes."))
